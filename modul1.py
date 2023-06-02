62128f2fe0f236df7b2d6ac30869671cb549c6a0
Poludnica <svasti.kaula@gmail.com>
2023-06-02 11:51:42 +0300

    add fib func and reserve_range

--- modul1.py
+++ modul1.py
@@ -2,3 +2,15 @@ def max1(a, b):
     if a > b:
         return a
     return b
+
+
+def LastFibo(n):
+    if n  in [0,1] :
+        return 1
+    return LastFibo(n-1) + LastFibo(n-2)
+
+def revers_range(num):
+    print(num, end = ' ')
+    if num > 0 :
+        revers_range(num-1)
+    
